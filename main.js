b335caef2ef404b94d5748a15bf0ef0c184a9377
misztijanos <misztijanos@yahoo.com>
2023-05-26 15:01:19 +0300

    finished

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
 import './scss/main.scss'
 
-let mobileButton = document.getElementsByClassName('header--mobile-button')[0]
+let mobileButton = document.getElementsByClassName('header--hamburger')[0]
+let logo = document.getElementsByClassName('header--logo')[0]
+let main = document.getElementsByTagName('main')[0]
+let footer = document.getElementsByTagName('footer')[0]
+let mobileMenu = document.getElementsByClassName('mobile-menu')[0]
+mobileButton.onclick = toggleMobileMenu
 
-mobileButton.onclick = () => {
+function toggleMobileMenu() {
   mobileButton.classList.toggle('clicked')
+  logo.classList.toggle('hide')
+  main.classList.toggle('hide')
+  footer.classList.toggle('hide')
+  mobileMenu.classList.toggle('show')
 }
